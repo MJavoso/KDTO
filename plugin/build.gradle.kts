afc4a76921a67c091b3b2c0c9000c7f3bbe41f7b
MJavoso <marcoapdr15@gmail.com>
2025-06-09 16:14:36 -0600

    Agregando plugin para subir mi plugin al portal de gradle, y tareas de maven para probarlo en local

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     id(libs.plugins.kotlin.jvm.get().pluginId)
     `kotlin-dsl`
+    `maven-publish`
+    signing
+    alias(libs.plugins.gradle.plugin.publish)
 }
 
 group = "io.github.mjavoso"
 version = "1.0.0-alpha01"
 
 dependencies {
-    implementation(project(":processor"))
     implementation(localGroovy())
     implementation(gradleApi())
     testImplementation(kotlin("test"))
@@ -27,10 +29,39 @@ repositories {
 }
 
 gradlePlugin {
+    website = "https://github.com/MJavoso/KDTO"
+    vcsUrl = "https://github.com/MJavoso/KDTO.git"
     plugins {
         create("kdtoPlugin") {
             id = "io.github.mjavoso.kdto.plugin"
+            displayName = "Plugin for the KDTO generator"
+            description = "Plugin that helps you configure automatically all the dependencies for KDTO class generator"
+            tags = listOf("kotlin", "kdto")
             implementationClass = "com.marcode.kdto.plugin.KDTOGradlePlugin"
         }
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = group.toString()
+            artifactId = "kdto-plugin"
+            version = version.toString()
+        }
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            name = "localPluginRepository"
+            url = uri(layout.buildDirectory.dir("local-plugin-repository"))
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+    useGpgCmd()
 }
