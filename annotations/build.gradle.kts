a01952dc61d697733d5ad565e42b62a33b076c3c
MJavoso <marcoapdr15@gmail.com>
2025-06-08 23:37:28 -0600

    Agregando jreleaser al modulo annotations

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jreleaser.model.Active
+
 plugins {
     id(libs.plugins.kotlin.jvm.get().pluginId)
     `maven-publish`
+    id(libs.plugins.jreleaser.get().pluginId)
 }
 
 group = "io.github.mjavoso"
@@ -32,9 +35,54 @@ publishing {
             group = pluginGroup
             artifactId = "kdto-annotations"
             version = pluginVersion
+
+            pom {
+                name.set("KDTO Annotations")
+                description.set("Annotation definitions for the KDTO code generation processor")
+                url.set("https://github.com/MJavoso/KDTO")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("MJavoso")
+                        name.set("Marco Pinedo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/MJavoso/KDTO")
+                    connection.set("scm:git:https://github.com/MJavoso/KDTO.git")
+                    developerConnection.set("scm:git:ssh://github.com/MJavoso/KDTO.git")
+                }
+            }
         }
     }
     repositories {
         mavenLocal()
+        maven {
+            url = uri(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+jreleaser {
+    gitRootSearch = true
+    signing {
+        active = Active.ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    setActive("ALWAYS")
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("target/staging-deploy")
+                }
+            }
+        }
     }
 }
