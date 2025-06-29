78126d48b9ddfc5acfabe25fe4e3b85c03c5663e
MJavoso <marcoapdr15@gmail.com>
2025-06-28 23:51:04 -0600

    Refine exception description in DtoDefinitionConflictException
    Updated the exception documentation to better clarify potential conditions that cause conflicts during DTO generation. Simplified explanations and removed examples for improved readability and focus.

--- annotations/src/main/kotlin/com/marcode/kdto/annotations/exceptions/DtoDefinitionConflictException.kt
+++ annotations/src/main/kotlin/com/marcode/kdto/annotations/exceptions/DtoDefinitionConflictException.kt
@@ -4,27 +4,15 @@ package com.marcode.kdto.annotations.exceptions
  * Exception thrown during DTO generation when there's a potential naming conflict between
  * the interface definition and its generated DTO class.
  *
- * This exception occurs when both of these conditions are met:
- * 1. The interface annotated with [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef]
+ * This exceptions occurs when any of these conditions are met:
+ * 1. A property is declared, but it is also excluded on the
+ * [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef] exclusion list.
+ * 2. When both of these condutions occur:
+ * - The interface annotated with [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef]
  *    is not marked as `private`
- * 2. No explicit `dtoName` is specified in the [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef]
+ *  - No explicit `dtoName` is specified in the [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef]
  *    annotation
  *
- * To resolve this issue, either:
- * - Mark the interface as `private`, or
- * - Specify an explicit `dtoName` in the [@DtoDef][com.marcode.kdto.annotations.definitions.DtoDef]
- *   annotation
- *
- * Example of valid definitions:
- * ```kotlin
- * // Option 1: Private interface
- * @DtoDef(sourceClass = User::class)
- * private interface UserView
- *
- * // Option 2: Public interface with explicit DTO name
- * @DtoDef(sourceClass = User::class, dtoName = "UserViewDto")
- * interface UserView
- * ```
  *
  * @see DtoDef
  */
