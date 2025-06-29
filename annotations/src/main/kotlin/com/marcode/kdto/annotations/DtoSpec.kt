bb6d946a44e5372953814bba4778528618087973
MJavoso <marcoapdr15@gmail.com>
2025-06-28 18:00:48 -0600

    Add includeAnnotations property to DtoSpec annotation
    This new property allows configuring whether class and property level annotations should be inherited by the generated DTO. It defaults to true, providing more flexibility for managing annotations in DTO processing.

--- annotations/src/main/kotlin/com/marcode/kdto/annotations/DtoSpec.kt
+++ annotations/src/main/kotlin/com/marcode/kdto/annotations/DtoSpec.kt
@@ -8,10 +8,14 @@ package com.marcode.kdto.annotations
  * @property dtoName The name of the generated DTO class.
  * @property include An optional list of property names to include in the DTO.
  * @property exclude An optional list of property names to exclude from the DTO.
+ * @property inheritAnnotations An optional boolean to tell the processor to include class and property level annotations. [true] by default.
+ *
+ * @throws com.marcode.kdto.annotations.exceptions.PropertyNotFoundException  if properties defined in [include] or [exclude] are not found on source class
  */
 @Retention(AnnotationRetention.BINARY)
 annotation class DtoSpec(
     val dtoName: String,
     val include: Array<String> = [],
-    val exclude: Array<String> = []
+    val exclude: Array<String> = [],
+    val includeAnnotations: Boolean = true
 )
