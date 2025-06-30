89d55eeb83ae208613ea33b3fa089258cd3499a7
MJavoso <marcoapdr15@gmail.com>
2025-06-30 13:25:12 -0600

    Add support for filtering annotations in DTO generation
    Introduce a `filteredAnnotations` list to include `DtoProperty` alongside `Dto` and `DtoDef`. This allows the `DTOGenerator` to filter and process symbols based on specific annotations, improving flexibility and enhancing processing capabilities.

--- processor/src/main/kotlin/com/marcode/kdto/processor/KDTOProcessor.kt
+++ processor/src/main/kotlin/com/marcode/kdto/processor/KDTOProcessor.kt
@@ -9,6 +9,7 @@ import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.marcode.kdto.annotations.Dto
 import com.marcode.kdto.annotations.definitions.DtoDef
+import com.marcode.kdto.annotations.definitions.DtoProperty
 import com.marcode.kdto.generator.DTOGenerator
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.KModifier
@@ -19,6 +20,7 @@ class KDTOProcessor(
     private val codeGenerator: CodeGenerator,
     private val logger: KSPLogger
 ): SymbolProcessor {
+    private val filteredAnnotations = listOf(Dto::class, DtoDef::class, DtoProperty::class)
     override fun process(resolver: Resolver): List<KSAnnotated> {
         processDtos(resolver)
         processDtoDefinitions(resolver)
@@ -30,7 +32,7 @@ class KDTOProcessor(
         val symbols = resolver.getSymbolsWithAnnotation(Dto::class.qualifiedName!!)
         symbols.filterIsInstance<KSClassDeclaration>().forEach { classDecl ->
             val dtoProcessor = DTOAnnotationProcessor(classDecl, logger)
-            val generator = DTOGenerator(dtoProcessor.dtoDeclarations, logger)
+            val generator = DTOGenerator(dtoProcessor.dtoDeclarations, logger, filterAnnotations = filteredAnnotations)
             generator.dtoFiles.forEach { fileSpec ->
                 val file = codeGenerator.createNewFile(
                     dependencies = Dependencies(false, classDecl.containingFile!!),
@@ -50,6 +52,19 @@ class KDTOProcessor(
         val symbols = resolver.getSymbolsWithAnnotation(DtoDef::class.qualifiedName!!)
         symbols.filterIsInstance<KSClassDeclaration>().forEach { classDecl ->
             val dtoDefinitonProcessor = DTODefinitionProcessor(classDecl, logger)
+            val generator = DTOGenerator(listOf(dtoDefinitonProcessor.dtoDefinition), logger, filterAnnotations = filteredAnnotations)
+            generator.dtoFiles.forEach { fileSpec ->
+                val file = codeGenerator.createNewFile(
+                    dependencies = Dependencies(false, classDecl.containingFile!!),
+                    packageName = fileSpec.packageName,
+                    fileName = fileSpec.name
+                )
+
+                OutputStreamWriter(file, Charsets.UTF_8).use { writer ->
+                    fileSpec.writeTo(writer)
+                    logger.info("Generated DTO: ${fileSpec.name} from ${classDecl.simpleName.asString()}")
+                }
+            }
         }
     }
 }
