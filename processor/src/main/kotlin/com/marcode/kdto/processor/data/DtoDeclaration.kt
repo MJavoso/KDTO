d4526bde1d7802090f44ead9c66968f09c6ae111
MJavoso <marcoapdr15@gmail.com>
2025-06-30 13:24:10 -0600

    Remove unused 'originalPackageName' property from DtoDeclaration
    The 'originalPackageName' property was redundant since 'packageName' content was the same.

--- processor/src/main/kotlin/com/marcode/kdto/processor/data/DtoDeclaration.kt
+++ processor/src/main/kotlin/com/marcode/kdto/processor/data/DtoDeclaration.kt
@@ -6,7 +6,6 @@ import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 internal data class DtoDeclaration(
     val packageName: String,
     val originalClassName: String,
-    val originalPackageName: String,
     val dtoName: String,
     val includedProperties: List<DtoProperty>,
     val classAnnotations: List<KSAnnotation>
